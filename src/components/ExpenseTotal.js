826b973b285c04474caec395b62bd38bdd4e094c
mazapander <98283345+mazapander@users.noreply.github.com>
2024-02-01 12:50:50 +0100

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import Currency from './currency';
 const ExpenseTotal = () => {
     const { expenses } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
@@ -7,7 +8,7 @@ const ExpenseTotal = () => {
     }, 0);
     return (
         <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+            <span>Spent so far: <span>{Currency}</span>{totalExpenses}</span>
         </div>
     );
 };
